d30faea0783c70d164a9eb5b3d2e8e68b7aeb012
vitor <vitor.moraes.cardoso@gmail.com>
2026-05-23 22:42:17 -0300

    Rename Class1 to User and implement user properties and methods; update project dependencies

---
+++ src/UserManagement.Domain/User.cs
@@ -0,0 +1,33 @@
+﻿namespace UserManagement.Domain;
+
+public class User
+{
+    public int Id { get; private set; }
+    
+    public string Nome { get; private set; } =  string.Empty;
+    public string Email { get; private set; } =  string.Empty;
+    public DateTime DataNascimento { get; private set; }
+    public DateTime DataCriacao { get; private set; }
+    public DateTime DataEdicao { get; private set; }
+    
+    private User() { }
+
+    public User(string nome, string email, DateTime dataNascimento)
+    {
+        ValidateAndCreate(nome, email, dataNascimento);
+        DataCriacao = DateTime.UtcNow;
+        DataEdicao = DateTime.UtcNow;
+    }
+    
+    public void Update(string nome, string email, DateTime dataNascimento)
+    {
+        ValidateAndCreate(nome, email, dataNascimento);
+        DataEdicao = DateTime.UtcNow;
+    }
+
+    private void ValidateAndCreate(string nome, string email, DateTime dataNascimento)
+    {
+        
+    }
+}
+
